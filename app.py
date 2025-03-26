b18ee3717979a6d4df0def9a1085223430acff16
Ariana Villa <arianavilla4817@gmail.com>
2025-03-26 11:37:23 -0300

    creamos rutas y links

--- app.py
+++ app.py
@@ -3,5 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def principal():
+    return """
+        <a href='/hola'>hola</a> 
+        <a href='/chau'>chau</a> 
+        """
+
+
+@app.route("/hola")
 def hello_world():
-    return "<p>holis</p>"
+    return "<p>hola</p>"
+
+@app.route("/chau")
+def despedir ():
+    return "<p>chau</p>"
+
+#dos rutas, dos links
