5cf80c94f56234c08e8a9ab2ed317b0ac50e92aa
Ariana Villa <arianavilla4817@gmail.com>
2025-04-30 12:12:13 -0300

    hicimos rutas, insertamos,borramos,mostramos,cambiamos(el de cambiar no lo termine

--- README.md
+++ README.md
@@ -3,10 +3,43 @@ python -m venv .venv
 source .venv/bin/activate
 pip install flask
 flask --version
-flask run: 
+
+flask run: <!--corre normal
 opciones para ver como se corre
 flask run -h 0.0.0.0
--- debug me permite ver lo errores y ver los cambios automaticamente 
--p 5010 difernte 5000
+flask run --debug <!--me permite ver lo errores y ver los cambios automaticamente 
+flask run -p 5010 diferente 5000
 
 crear carpeta
+
+
+#tipos de ruta
+#absoluto: con barra
+#relativo: sin barra 
+
+cursor.execute()
+cursor.fetchone() <!-- proximo registro, devuelve uno solo
+cursor.fetchall() <!-- todos los registros que quedan en una lista 
+
+db.commit()<!--confirma los cambios>
+db.cursor()<!--pide un cursor nuevo>
+db.close()<!--cierra>
+db.execute() <!--toma la consulta y parametros >
+
+SQLITE:
+delete from usuarios WHERE id = 2 <!-- le digo que usuario quiero borrar, si no pongo el WHERE se borra todo>
+
+usr = 2
+consulta = f"delete from usuarios WHERE id = {usr};"
+consulta
+
+user =2 OR 1=1
+consulta = f"delete from usuarios WHERE id = {usr};"<!--borra todo, por eso no se usa la "f">
+consulta
+
+manera de como si hacer la consulta:
+cursor = db.execute("delete from usuarios WHERE id=?", (usr,))<!-- siempre con un signo de pregunta y una tupla>
+dir(cursor)
+
+
+cursor sirve para ver los resultados
--- app.py
+++ app.py
@@ -69,43 +69,113 @@ def main ():
     <br>
     <a href="{url_dado}">Tirar dado</a>
     """
-#-------------CONEXION A BASE DE DATOS---------------------------------#
-db=None #variable vacia
+#-------------CONEXION A BASE DE DATOS---------------------------------con thomi#
+#db=None #variable vacia
+
+#def abrirConexion():
+#    global db  # llama 
+#   db = sqlite3.connect('instance/datos.sqlite') #abre lo que le ponemos dentro del parentesis
+#   db.row_factory = sqlite3.Row
+#   return db # devuelve
+
+#def cerrarConexion():
+#    global db #llama
+#    if db is not None:#preunta si no esta vacio
+#       db.close()#cierra
+#        db=None#vuelve a estar vacio
+
+#@app.route("/usuarios/")
+#def obterGente():
+#    global db 
+#    conexion = abrirConexion()
+#    cursor = conexion.cursor()
+#    cursor.execute('SELECT * FROM usuarios')
+#    resultado = cursor.fetchall()#fetchall es como el asterisco,selecciona todo en cambio el fetchone solo uno
+#    cerrarConexion()
+#    fila = [dict(row) for row in resultado] 
+#    return str(fila)
+#-------------------------------------------------
+db = None
+
+
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
 
 def abrirConexion():
-    global db  # llama 
-    db = sqlite3.connect('instance/datos.sqlite') #abre lo que le ponemos dentro del parentesis
-    db.row_factory = sqlite3.Row
-    return db # devuelve
+   global db
+   db = sqlite3.connect("instance/datos.sqlite")
+   db.row_factory = dict_factory
+
 
 def cerrarConexion():
-    global db #llama
-    if db is not None:#preunta si no esta vacio
-        db.close()#cierra
-        db=None#vuelve a estar vacio
-
-@app.route("/usuarios/")
-def obterGente():
-    global db 
-    conexion = abrirConexion()
-    cursor = conexion.cursor()
-    cursor.execute('SELECT * FROM usuarios')
-    resultado = cursor.fetchall()#fetchall es como el asterisco,selecciona todo en cambio el fetchone solo uno
-    cerrarConexion()
-    fila = [dict(row) for row in resultado] 
-    return str(fila)
-
-  
+   global db
+   db.close()
+   db = None
 
 
+@app.route("/test-db")
+def testDB():
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")
+   res = cursor.fetchone()
+   registros = res["cant"]
+   cerrarConexion()
+   return f"Hay {registros} registros en la tabla usuarios"
+
+#actividades
+
+#insertar un usuario nuevo 
+@app.route("/insertar/<string:usuario>/<string:email>")#nombre, email
+def insertar_usuario(usuario,email):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor = db.execute ("INSERT INTO usuarios(usuario, email) VALUES('mari', 'm@gmail.com');")
+   cursor = db.commit()
+   cerrarConexion()
+   return f"se agrego a {usuario} y con el email {email} "
+
+#borrar un usuario
+@app.route("/borrar/<int:id>")#detalle/id
+def borrar_usuario(id):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor = db.execute ("DELETE from usuarios WHERE id=?", (id,))
+   cursor = db.commit()
+   cerrarConexion()
+   return f"se borro a id {id} en la tabla de usuarios"
+
+#mostrar un usuario y un email
+@app.route("/mostrar/<int:id>")#id
+def mostrar_usuario(id):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor = db.execute ("SELECT usuario, email from usuarios WHERE id=?", (id,))
+   resultado = cursor.fetchone()
+   cerrarConexion()
+   return f"usuario:{resultado['usuario']}    email:{resultado['email']}"
+
+#cambiar el mail de un usuario elegido, no lo termine
+@app.route("/cambiar/<string:usuario>/<string:email>")#nombre, email
+def cambiar_email(usuario, email):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor = db.execute ("SELECT usuario, email from usuarios WHERE id=?")
+   resultado = cursor.fetchone()
+   cerrarConexion()
+   return f"se le cambio el email a   quedo como :{resultado['usuario']}    email:{resultado['email']}"
 
 
 
 
 
-#tipos de ruta
-#absoluto: con barra
-#relativo: sin barra 
+
+
+
+
 
 
 
